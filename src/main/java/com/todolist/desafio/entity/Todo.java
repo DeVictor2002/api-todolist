6a15089e3b964627d3ad9ecbb5e080e0af1c617a
Victor <victorhenriquebl11@gmail.com>
2024-12-03 19:58:38 -0300

    refactor: field id to Long

--- src/main/java/com/todolist/desafio/entity/Todo.java
+++ src/main/java/com/todolist/desafio/entity/Todo.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String title;
     private String description;
     private boolean done;
